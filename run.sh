adc629cf4b3c1e32ad8809f9af82404d3a9090b2
Sam Gross <colesbury@gmail.com>
2017-03-03 19:32:12 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-wget -O ngrok.zip https://dl.ngrok.com/ngrok_2.0.19_linux_amd64.zip
+wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok.zip
 {
     mkfifo pipe
+    echo "Executing nc"
     nc -l -v 8888 <pipe | bash >pipe
     killall -SIGINT ngrok && echo "ngrok terminated"
 } &
 {
-    ./ngrok tcp 8888 --authtoken=$NGROK_TOKEN --log=stdout --log-level=debug | grep "tcp.ngrok.io" || true
+    echo "Executing ngrok"
+    ./ngrok authtoken $NGROK_TOKEN
+    ./ngrok tcp 8888 --log=stdout --log-level=debug | grep "tcp.ngrok.io" || true
 } &
