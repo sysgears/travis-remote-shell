17bcb577c6c62acd59d1a8b2fa3ca49d0ccb81a5
Sam Gross <colesbury@gmail.com>
2017-03-03 19:18:24 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ wget -O ngrok.zip https://dl.ngrok.com/ngrok_2.0.19_linux_amd64.zip
 unzip ngrok.zip
 {
     mkfifo pipe
-    nc -l -v -c 8888 <pipe | bash >pipe
+    nc -l -v 8888 <pipe | bash >pipe
     killall -SIGINT ngrok && echo "ngrok terminated"
 } &
 {
